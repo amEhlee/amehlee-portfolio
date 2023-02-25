2c280d8b2870e46fdbd611a8834cccec6fed3364
AmEhlee <ElieKabengele.Work@gmail.com>
2023-02-25 05:11:55 -0700

    add dist

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
 export default defineConfig({
+  base:"/amehlee-portfolio",
   plugins: [reactRefresh()],
   server: {
     open: true,
